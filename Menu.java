a2764a3710fda46fcbeb60ac060a77f46618a3cc
Persiee <116552606+Persiee@users.noreply.github.com>
2023-01-23 18:42:49 +0100

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,20 +1,28 @@
 package Ulohy.dryha.serhii;
 
 import java.util.Scanner;
+/** 
+ * This is a menu for semestral work
+ * @author Serhii Dryha
+ */  
 public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("vybrat ukol:\n1 Semestralni Prace\n2 Vanocni Uloha\n0 vyhod");
-        int a=scanner.nextInt();
-        switch(a){
-            case 1:
-                Uloha1DryhaSerhii.main(args);
-                break;
-            case 2:
-                VanocniUlohyDryhaSerhii.main(args);
-                break;
-            default:
-                break;
+        boolean end = false;
+        while(end==false){
+            System.out.println("Vybrat ukol:\n1 Semestralni Prace\n2 Vanocni Uloha\n0 vyhod");
+            int a=scanner.nextInt();
+            switch(a){
+                case 1:
+                    Uloha1DryhaSerhii.main(args);
+                    break;
+                case 2:
+                    VanocniUlohyDryhaSerhii.main(args);
+                    break;
+                default:
+                    end=true;
+                    break;
+            }
         }
     }
 }
--- Uloha1DryhaSerhii.java
+++ Uloha1DryhaSerhii.java
@@ -1,7 +1,7 @@
 package Ulohy.dryha.serhii;
 import java.util.Scanner;
 /** 
- * 1. Program searches for the longest sorted subsequence in a given sequence and the sequence start index
+ * 1. Method searches for the longest sorted subsequence in a given sequence and the sequence start index
  * @author Serhii Dryha
  * @version 1 21/12/22 
  */  
@@ -18,10 +18,7 @@ public class Uloha1DryhaSerhii {
             System.out.println("Zadejte posloupnost:");
             int sequence[]=new int[size];
             int LastNum=Integer.MIN_VALUE;
-            int MaxLenght=0;
-            int StartIndex=1;
-            int tempML=0;
-            int tempSI=0;
+            int MaxLenght=0,tempML=0,tempSI=0,StartIndex=1;
             for (int i = 0; i < size; i++) {
                 //checking every number if it bigger than previous number
                 sequence[i]=sc.nextInt();
